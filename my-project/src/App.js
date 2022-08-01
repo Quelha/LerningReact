6accec8302f44d505f504e93042e3e84410ce784
Quelha <felipequelha@gmail.com>
2022-08-01 17:11:05 +0000

    FirstCommit

---
+++ my-project/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  const name = 'Felipe'
+  
+
+  return (
+    <div className="App">
+      <h1> Olá, mundo</h1>
+      <p>Meu nome é {name}</p>
+    </div>
+  );
+}
+
+export default App;
