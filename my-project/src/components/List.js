30030256b62099881bb8840ab437da621dc878fb
Quelha <felipequelha@gmail.com>
2022-08-02 13:30:26 +0000

    FinalAula08

--- my-project/src/components/List.js
+++ my-project/src/components/List.js
@@ -6,9 +6,9 @@ function List() {
         <>
             <h1>Minha lista</h1>
             <ul>
-                <Item marca="Voltz"/>
-                <Item marca="Honda"/>
-                <Item marca="Yamaha"/>
+                <Item marca="Voltz" lancamento={2020}/>
+                <Item marca="Honda" lancamento={1970}/>
+                <Item lancamento={1972}/>
             </ul>
         </>
     )
